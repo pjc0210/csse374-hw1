5fc0030a949e7cb2d3b8e513f27bd2c6624a34c1
PJ <75004958+pjc0210@users.noreply.github.com>
2025-12-13 11:10:36 -0500

    Added back main class

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Singer> songToSinger = new HashMap<String, Singer>();
+        HashMap<Integer, Singer> singer = new HashMap<Integer, Singer>();
+
+        // populate HashMaps with sample data
+        Singer s1 = new Singer(1, "Mason", "\"Can't Help Falling in Love\" by Elvis Presley");
+        Singer s2 = new Singer(2, "PJ", "\"At Last\" by Etta James");
+        Singer s3 = new Singer(3, "Niko", "\"Ballad of a homeschooled girl\" by Olivia Rodrigo");
+        songToSinger.put(s1.getSongName(), s1);
+        songToSinger.put(s2.getSongName(), s2);
+        songToSinger.put(s3.getSongName(), s3);
+        singer.put(s1.getId(), s1);
+        singer.put(s2.getId(), s2);
+        singer.put(s3.getId(), s3);
+
+        ProgramManager pm = new ProgramManager(songToSinger, singer);
+        Scanner scanner = pm.getScanner();
+
+        // Main I/O selecting logic
+        while(true){
+            System.out.println("Welcome to the Jabbering Jen's Music Club Valentine's Song System!");
+            System.out.println("""
+                Select an action:
+                1 - Customers – Order a song for Valentine’s Day
+                2 – Club members – Get a report of requests for your song
+                3 – Club members – Report back that your songs are done
+                4 - Admin - Show data for all club members
+                Enter 1, 2, 3 or 4:  """);
+            String line = scanner.nextLine().trim();
+            if (line.equals("exit")) {
+                break;
+            }
+
+            int input = -1;
+            try {
+                input = Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                continue;
+            }
+
+            if (input > 4 || input < 1){
+                System.out.println("Invalid input, please try again");
+            } else {
+                pm.selectAction(input);
+                System.out.println();
+            }
+        }
+    }
+}
--- src/ProgramManager.java
+++ src/ProgramManager.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ProgramManager {
@@ -234,51 +233,5 @@ public class ProgramManager {
 
         Pattern p = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
         return email != null && p.matcher(email).matches();
-    }
-
-    public static void main(String[] args) {
-        HashMap<String, Singer> songToSinger = new HashMap<String, Singer>();
-        HashMap<Integer, Singer> singer = new HashMap<Integer, Singer>();
-
-        // populate HashMaps with sample data
-        Singer s1 = new Singer(1, "Mason", "\"Can't Help Falling in Love\" by Elvis Presley");
-        Singer s2 = new Singer(2, "PJ", "\"At Last\" by Etta James");
-        Singer s3 = new Singer(3, "Niko", "\"Ballad of a homeschooled girl\" by Olivia Rodrigo");
-        songToSinger.put(s1.getSongName(), s1);
-        songToSinger.put(s2.getSongName(), s2);
-        songToSinger.put(s3.getSongName(), s3);
-        singer.put(s1.getId(), s1);
-        singer.put(s2.getId(), s2);
-        singer.put(s3.getId(), s3);
-
-        ProgramManager pm = new ProgramManager(songToSinger, singer);
-        Scanner scanner = pm.getScanner();
-
-        // Main I/O selecting logic
-        while(true){
-            System.out.println("Welcome to the Jabbering Jen's Music Club Valentine's Song System!");
-            System.out.println("""
-                Select an action:
-                1 - Customers – Order a song for Valentine’s Day
-                2 – Club members – Get a report of requests for your song
-                3 – Club members – Report back that your songs are done
-                4 - Admin - Show data for all club members
-                Enter 1, 2, 3 or 4:  """);
-            String line = scanner.nextLine().trim();
-            int input = -1;
-            try {
-                input = Integer.parseInt(line);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input. Please enter a valid number.");
-                continue;
-            }
-
-            if (input > 4 || input < 1){
-                System.out.println("Invalid input, please try again");
-            } else {
-                pm.selectAction(input);
-                System.out.println();
-            }
-        }
     }
 }
